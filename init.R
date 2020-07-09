6fc9fd4ffdc09c0dfd054ae9863a4f1bd59f45c7
sagar-aps <sagaraps1@gmail.com>
2020-07-09 21:57:45 +0200

    Figured out using a corpus, need to see how to reintegrate it into the df

--- init.R
+++ init.R
@@ -12,45 +12,71 @@ library(tm)
 library(snowballc)
 library(wordcloud)
 library(RColorBrewer)
+
+
 colnames(full_data)
+data[1,]
 
 meanchar <- full_data %>% group_by(username) %>% summarise(mean(stri_count_words(username)))
-meanchar
-
-stri_count_words("Does this work")
 
 full_data %>% group_by(username)%>% summarise(stri_count_fixed(text, " "))
-
 
 head(full_data %>% mutate(noofwords = stri_count_fixed(text, " ")))
 
 data <-full_data %>% mutate(noofwords = stri_count_fixed(text, " "))
+#data to avoid messing up original dataset
+
 meanchar  <- data %>% group_by(username) %>% summarise(mean(noofwords))
 
-
+#Text Cleaning Section
 #Replacing "/", "@" and "|" with space
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 str(data)
 
 data <- data %>% mutate_at("text",as.character) #Converting text from factor to character
 
-data<- data %>% mutate(cleantext = tm_map(text, toSpace, "/") ) #Unsure of how this works but it does
+#data<- data %>% mutate(cleantext = tm_map(text, toSpace, "/") ) #Unsure of how this works but it doesn't
+corp <- Corpus(VectorSource(data$text))
 
-TextDoc <- tm_map(TextDoc, toSpace, "/")
-TextDoc <- tm_map(TextDoc, toSpace, "@")
-TextDoc <- tm_map(TextDoc, toSpace, "\\|")
 # Convert the text to lower case
-TextDoc <- tm_map(TextDoc, content_transformer(tolower))
-# Remove numbers
-TextDoc <- tm_map(TextDoc, removeNumbers)
+corp <- tm_map(corp, content_transformer(tolower))
+
+# remove punctuation
+corp <- tm_map(corp, removePunctuation)
+
 # Remove english common stopwords
-TextDoc <- tm_map(TextDoc, removeWords, stopwords("english"))
-# Remove your own stop word
-# specify your custom stopwords as a character vector
-TextDoc <- tm_map(TextDoc, removeWords, c("s", "company", "team")) 
-# Remove punctuations
-TextDoc <- tm_map(TextDoc, removePunctuation)
+corp <- tm_map(corp, removeWords, stopwords("english"))
+
 # Eliminate extra white spaces
-TextDoc <- tm_map(TextDoc, stripWhitespace)
+corp <- tm_map(corp, stripWhitespace)
+
 # Text stemming - which reduces words to their root form
-TextDoc <- tm_map(TextDoc, stemDocument)
+corp <- tm_map(corp, stemDocument)
+
+corp <- tm_map(corp, toSpace, "/")
+corp <- tm_map(corp, toSpace, "@")
+corp <- tm_map(corp, toSpace, "\\|")
+
+#specify your custom stopwords as a character vector and remove your own stop word
+corp <- tm_map(corp, removeWords, c("hrs", "govt", "days")) 
+
+
+# inspect the first 5 documents (tweets) inspect(myCorpus[1:5]) 
+# The code below is used for to make text fit for paper width 
+for (i in 1:5) {
+  cat(paste("[[", i, "]] ", sep = ""))
+  #writeLines(myCorpus[[i]])
+  writeLines(as.character(corp[[i]]))
+}
+
+
+
+
+
+
+
+
+
+
+
+
